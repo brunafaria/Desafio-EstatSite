1ffef59c7b7442fd4641840c1471efbce3414fc7
Bruna Faria <brunaffaria93@gmail.com>
2020-06-06 20:23:01 -0300

    add others exercises

--- scrip_fifa19.R
+++ scrip_fifa19.R
@@ -9,6 +9,8 @@ rm(list=ls(all=T))
 require(dplyr)
 require(ggplot2)
 require(stringr)
+require(Hmisc)
+require(fastDummies)
 
 #---- Leitura base de dados
 
@@ -120,3 +122,123 @@ df_fifa %>%
 df_fifa %>%
   ggplot(aes(WageAux)) +
   geom_histogram(fill="skyblue4", alpha=0.5, position="identity")
+
+
+#---- Data set iris
+
+df_iris = read.csv2('Iris.csv', sep = ',')
+df_iris %>% head()
+df_iris %>% dim()
+
+#---- Q7
+
+# Através de um gráfico de dispersão (scatterplot), 
+# verifique se há relação linear entre comprimento da pétala (Petal Length) 
+# e o comprimento da sépala (Sepal Length). 
+# Adicione também diferentes cores aos pontos de acordo com a espécie da flor. 
+# A resposta aqui é somente o gráfico, não se preocupe em fazer análises mais aprofundadas.
+
+# R.: Há correlação significativa e positiva entre 'sepal length' e 'petal length' das espécies versicolor e 
+# virginia. A correlação da espécie setosa se motrou marginalmente significativa.
+
+ggplot(df_iris,aes(x=as.numeric(as.character(df_iris$SepalLengthCm)), 
+                   y=as.numeric(as.character(df_iris$PetalLengthCm)), colour=Species)) +
+  geom_point() +
+  xlab("Sepal Length (cm)") +
+  ylab("Petal Length (cm)") +
+  theme_classic() +
+  ggtitle("Correletion between sepal length and petal length")
+
+table_corr = df_iris %>% 
+  mutate(SepalLengthCm = as.numeric(as.character(SepalLengthCm)),
+         PetalLengthCm = as.numeric(as.character(PetalLengthCm))) %>% 
+  group_by(Species) %>% 
+  group_map(~rcorr(cbind(.$SepalLengthCm, .$PetalLengthCm), type="spearman")) %>% 
+  do.call(rbind, .) 
+
+#---- Q8 
+
+# Primeiro, apague a substring “Iris-” da coluna Species. Em seguida, adicione 3 novas colunas 
+# à tabela inicial, sendo que cada coluna receberá uma dummy referente a cada uma das species. 
+# Ou seja, você deve criar uma coluna chamada Dummy_Setosa, que recebe 1 se a flor for da espécie 
+# Setosa e 0 caso contrário. O mesmo para as demais espécies.
+
+df_iris %>% 
+  mutate(Species = gsub('Iris-', '', as.character(.$Species))) %>% 
+  dummy_cols(., select_columns = "Species")
+
+#---- Data set MGLU3 e lren3
+
+df_mglu = read.csv2('MGLU3.SA.csv', sep = ',')
+df_lren = read.csv2('LREN3.SA.csv', sep = ',')
+
+df_mglu %>% head()
+df_lren %>% head()
+
+df_mglu %>% dim()
+df_lren %>% dim()
+
+#---- Q9 
+
+# Mostre através de um gráfico de linhas a evolução do preço de fechamento 
+# das duas ações durante os anos de 2017, 2018 e 2019. No mesmo gráfico, 
+# trace um gráfico de linhas pontilhadas com a evolução do preço de abertura 
+# das duas ações no mesmo período. Utilize cores diferentes para cada linha e insira uma 
+# legenda para as cores/linhas. A legenda deve ficar no canto inferior direito, como este exemplo:
+
+df_mglu %>% 
+  mutate(Acao = rep('MGLU3', dim(.)[1])) -> df_mglu
+
+df_lren %>% 
+  mutate(Acao = rep('LREN3', dim(.)[1])) -> df_lren
+
+
+df_acoes = rbind(df_mglu, df_lren)
+
+df_acoes_aux_lren = df_acoes %>% 
+  mutate(Date2 = format(as.Date(.$Date), "%Y-%m"), 
+         DateMes = format(as.Date(.$Date), "%m"), 
+         Ano_Filter = as.numeric(format(as.Date(.$Date), "%Y")),
+         Year = as.factor(format(as.Date(.$Date), "%Y")),
+         Close = as.numeric(as.character(Close)),
+         Open = as.numeric(as.character(Open))) %>% 
+  dplyr::filter(Ano_Filter > 2016 & Ano_Filter < 2020 & Acao == 'LREN3') %>% 
+  group_by(Date2, DateMes, Year, Acao) %>% 
+  summarise(Media_Open = mean(Open, na.rm = T),
+            Media_Close = mean(Close, na.rm = T)) 
+
+df_acoes_aux_mglu = df_acoes %>% 
+  mutate(Date2 = format(as.Date(.$Date), "%Y-%m"), 
+         DateMes = format(as.Date(.$Date), "%m"), 
+         Ano_Filter = as.numeric(format(as.Date(.$Date), "%Y")),
+         Year = as.factor(format(as.Date(.$Date), "%Y")),
+         Close = as.numeric(as.character(Close)),
+         Open = as.numeric(as.character(Open))) %>% 
+  dplyr::filter(Ano_Filter > 2016 & Ano_Filter < 2020 & Acao == 'MGLU3') %>% 
+  group_by(Date2, DateMes, Year, Acao) %>% 
+  summarise(Media_Open = mean(Open, na.rm = T),
+            Media_Close = mean(Close, na.rm = T)) 
+
+windows(10, 6)
+ggplot(df_acoes_aux_lren, aes(x = DateMes, y = Media_Close, group = Year)) +
+  theme_classic() +
+  geom_line(aes(color=Year), size=1)+
+  geom_point(aes(color=Year)) +
+  xlab('Close') +
+  ylab('Month') +
+  geom_line(aes(y = Media_Open, color=Year), size=1, linetype = "dashed") +
+  theme(legend.position= c(0.9, 0.15))
+
+
+windows(10, 6)
+ggplot(df_acoes_aux_mglu, aes(x = DateMes, y = Media_Close, group = Year)) +
+  theme_classic() +
+  geom_line(aes(color=Year), size=1)+
+  geom_point(aes(color=Year)) +
+  xlab('Close') +
+  ylab('Month') +
+  geom_line(aes(y = Media_Open, color=Year), size=1, linetype = "dashed") 
+
+
+
+
